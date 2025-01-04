dc4774f46700cd8ecd3a1a75f7fad1495b712c99
Nurlybek Nurzhan <136187061+Nurlybek-Nurzhan@users.noreply.github.com>
2025-01-04 22:00:33 +0500

    Initializing 2

---
+++ Assignments/src/Assignment_2/Task_1_1.java
@@ -0,0 +1,13 @@
+package Assignment_2;
+
+public class Task_1_1 {
+  public static void main(String[] args) {
+    String name = "John";
+    int age = 0;
+    String city = "New York";
+
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("City: " + city);
+  }
+}
