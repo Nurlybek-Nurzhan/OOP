f8bbfd8df08e7a5a8a86189ba9cb2d464d0a025e
Nurlybek Nurzhan <136187061+Nurlybek-Nurzhan@users.noreply.github.com>
2025-01-23 17:38:49 +0500

    fuck

--- Assignments/src/Assignment_3/Task_19.java
+++ Assignments/src/Assignment_3/Task_19.java
@@ -1,5 +1,14 @@
 package Assignment_3;
 
 public class Task_19 {
-  
+  public class Skyscraper {
+    public Skyscraper() {
+      System.out.println("Skyscraper built.");
+    }
+
+    public static void main(String[] args) {
+      Task_19 task = new Task_19();
+      task.new Skyscraper();
+    }
+  }
 }
--- Assignments/src/Assignment_3/Task_20.java
+++ Assignments/src/Assignment_3/Task_20.java
@@ -1,5 +1,23 @@
 package Assignment_3;
 
 public class Task_20 {
-  
+  public class Skyscraper {
+    public static final String SKYSCRAPER_WAS_BUILD = "Skyscraper is built.";
+    public static final String SKYSCRAPER_WAS_BUILD_FLOORS_COUNT = "Skyscraper is built. The number of floors - ";
+    public static final String SKYSCRAPER_WAS_BUILD_DEVELOPER = "Skyscraper is built. Developer - ";
+
+    public Skyscraper() {
+      System.out.println(SKYSCRAPER_WAS_BUILD);
+    }
+
+    public Skyscraper(int floorsCount) {
+      System.out.println(SKYSCRAPER_WAS_BUILD_FLOORS_COUNT + floorsCount);
+    }
+
+    public Skyscraper(String developer) {
+      System.out.println(SKYSCRAPER_WAS_BUILD_DEVELOPER + developer);
+    }
+  }
 }
+
+
